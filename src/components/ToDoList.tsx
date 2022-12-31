dc852d174cc98dee40b8ffb5cf3d55b3f6008d71
jongwonp <jw032876@gmail.com>
2022-12-31 15:33:48 +0900

    Refactor: atom, ToDo생성 폼, ToDoList 분리

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,22 @@
+import { useRecoilValue } from 'recoil';
+import { toDoState } from '../atom';
+import CreateToDo from './CreateToDo';
+
+function ToDoList() {
+  const toDos = useRecoilValue(toDoState);
+
+  return (
+    <div>
+      <h1>To Dos</h1>
+      <hr />
+      <CreateToDo />
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
